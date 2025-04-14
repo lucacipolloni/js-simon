ab645ca8af105db88da63392f5f968531987a795
lucacipolloni <lucacipolloni29@gmail.com>
2025-04-14 16:52:34 +0200

    add numbers

--- js/script.js
+++ js/script.js
@@ -5,6 +5,22 @@ const instructionsEl = document.getElementById("instructions");
 const numbersList = document.getElementById("numbers-list");
 const answersForm = document.getElementById("answers-form");
 
+// # RANDOM NUMBER GENERATION
+
+const generateRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const randomNumbers = [];
+
+while (randomNumbers.length < 5) {
+  const generatedNumber = generateRandomNumber(1, 50);
+  if (!randomNumbers.includes(generatedNumber)) {
+    randomNumbers.push(generatedNumber);
+    numbersList.innerHTML += `<li>${generatedNumber}</li>`;
+  }
+}
+
 // # COUNTDOWN
 
 let secondsLeft = 5;
@@ -14,6 +30,7 @@ const handleCountdownTick = () => {
   if (secondsLeft <= 0) {
     secondsLeft = 0;
     clearInterval(clock);
+    numbersList.classList.add("d-none");
     answersForm.classList.remove("d-none");
     countdownEl.classList.add("d-none");
     instructionsEl.innerText =
